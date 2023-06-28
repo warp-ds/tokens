f6b02ce6238aa6fc259b4df94ba0db9b80c23efd
BalbinaK <balbuhhha@gmail.com>
2023-06-28 10:44:40 +0200

    fix(index.js): process @import rule to inline font definitions in brand.css files

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { minify } from './css-minify.js'
 import drnm from 'drnm'
+import postcss from 'postcss'
+import atImport from 'postcss-import'
 
 const __dirname = drnm(import.meta.url)
 const outPath = path.join(__dirname, './dist')
 fs.mkdirSync(outPath, { recursive: true })
 
-function process(tld) {
+async function process(tld) {
+  const slugifiedName = slugify(tld)
+  const filename = path.join(outPath, slugifiedName) + '.css';
+
+  const fontDefinitions = `@import "@warp-ds/fonts/${slugifiedName}.css";`
   const tokens = tokenize(`./tokens/${tld}`);
-  const css = minify(tokens);
-  const filename = path.join(outPath, slugify(tld)) + '.css';
-  const fontsImport = `@import'https://assets.finn.no/pkg/@warp-ds/fonts/v1/${path.basename(filename)}';`
-  fs.writeFileSync(filename, `${fontsImport}${css}`, 'utf-8');
+  const css = `${fontDefinitions}${tokens}`;
+
+  const plugins = [
+    atImport
+  ]
+  const result = await postcss(plugins).process(css, { from: undefined, to: filename })
+
+  fs.writeFileSync(filename, minify(result.css), { encoding: 'utf-8' })
 }
 
 process('finn.no')
